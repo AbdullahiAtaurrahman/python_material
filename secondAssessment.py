98202135e184bcef151a2f763ff3b9cd4d6115f7
Ataur <atauraahmad@gmail.com>
2025-11-27 00:25:51 +0100

    create Car%Scooter classes

--- secondAssessment.py
+++ secondAssessment.py
@@ -1,9 +1,35 @@
-class User:
-    def __init__(self, firstName, lastName, age, regNumber):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.age = age
-        self.regNumber = regNumber
+class Vehicle:
+    def __init__(self, make, model, year, mileage = 0):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.__mileage = mileage
 
-    def Name(self):
-        return f"My name is ${self.firstName} ${self.lastName}"
+    def drive(self, distance):
+        self.__mileage += distance
+
+    def get_info(self):
+        return f"This is a {self.make}, {self.model}, a {self.year} model with {self.__mileage}"
+    
+    def __str__(self):
+        pass
+
+
+class Car(Vehicle):
+    vehicle_type = "Car"
+    def __init__(self, make, model, year, fuel_capacity, mileage=0):
+        super().__init__(make, model, year, mileage)
+        self.fuel_capacity = fuel_capacity
+    
+    def  get_info(self):
+        return f"This is a {self.make}, {self.model}, a {self.year} model with {self.__mileage} and {self.fuel_capacity} litres fuel capacity"
+    
+
+class ElectricScooter(Vehicle):
+    def __init__(self, make, model, year, battery_percentage, mileage=0):
+        super().__init__(make, model, year, mileage)
+        self.battery_percentage = battery_percentage
+    
+    def  get_info(self):
+        return f"This is a {self.make}, {self.model}, a {self.year} model with {self.__mileage} and {self.battery_percentage} litres fuel capacity"
+    
