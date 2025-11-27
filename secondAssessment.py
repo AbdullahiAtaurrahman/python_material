2aa7a25a08c3b2a6ca6ed747afe5bff818026280
Ataur <atauraahmad@gmail.com>
2025-11-27 01:22:23 +0100

    add static methods to Electric Scooter

--- secondAssessment.py
+++ secondAssessment.py
@@ -12,8 +12,11 @@ class Vehicle:
         return f"This is a {self.make}, {self.model}, a {self.year} model with {self.__mileage}"
     
     def __str__(self):
-        pass
-
+        return (
+            f"{self.year} {self.make} {self.model} | "
+            f"Mileage: {self.mileage} km | "
+            f"Battery: {self.battery_percentage}%"
+        )
 
 class Car(Vehicle):
     vehicle_type = "Car"
@@ -21,7 +24,7 @@ class Car(Vehicle):
         super().__init__(make, model, year, mileage)
         self.fuel_capacity = fuel_capacity
     
-    def  get_info(self):
+    def get_info(self):
         return f"This is a {self.make}, {self.model}, a {self.year} model with {self.__mileage} and {self.fuel_capacity} litres fuel capacity"
     
 
@@ -30,6 +33,10 @@ class ElectricScooter(Vehicle):
         super().__init__(make, model, year, mileage)
         self.battery_percentage = battery_percentage
     
-    def  get_info(self):
-        return f"This is a {self.make}, {self.model}, a {self.year} model with {self.__mileage} and {self.battery_percentage} litres fuel capacity"
-    
+    def drive(self, distance):
+        self.battery_percentage -= distance
+        if self.battery_percentage < 0:
+            self.battery_percentage = 0
+
+    def is_charging_required(battery_percentage):
+        return battery_percentage < 20
